b41a80a472fb5bf36abdbb9438b4640cf0bdd6d9
IAmEnchant <6639434+IAmEnchant@users.noreply.github.com>
2025-03-29 09:18:20 +0900

    chore: 프로젝트 초기화

---
+++ src/test/java/com/assignment/coordi/CoordiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.coordi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoordiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
